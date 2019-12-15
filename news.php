9303a66c297bb3f5fdc6734a1a739f79b7212698
DangNguyen-maker <github.com/DangNguyen-maker/1551060773.git>
2019-12-15 22:07:31 +0700

    Them file trang tin tuc

---
+++ news.php
@@ -0,0 +1,33 @@
+	<div class="container news">
+				<h3>Tin Tức Mới CSE</h3>
+				<ul>
+					<li>
+						<img src="images/news/new1.jpg" alt="Ảnh DEMO">
+						<div>
+							<h4><a href="">Công bố quyết định của Hiệu trưởng Bổ nhiệm viên chức quản lý cấp bộ môn khoa CNTT</a></h4>
+							<p>Trong những ngày đầu tháng 1 năm 2019, thừa ủy quyền của Hiệu trưởng trường Đại học Thủy Lợi, Đảng ủy - Ban Chủ nhiệm khoa Công nghệ thông tin đã tiến hành trao quyết định bổ nhiệm chức vụ Trưởng Bộ môn Toán học cho TS Nguyễn Hữu Thọ, Phó trưởng Bộ môn Toán học cho TS Đỗ Lân, Phó trưởng Bộ môn Hệ thống thông tin đối với TS Trần Mạnh Tuấn, Khoa Công nghệ thông tin.</p>
+						</div>
+					</li>
+					<li>
+						<img src="images/news/new2.jpg" alt="Ảnh DEMO">
+						<div>
+							<h4><a href="">Sinh viên Khoa CNTT giao lưu với công ty Qualica Nhật Bản</a></h4>
+							<p>(TLU) – Chiều ngày 23/04/2018, tại Trường Đại học Thủy lợi diễn ra buổi giao lưu giữa sinh viên Khoa CNTT với các lãnh đạo và nhân viên của công ty Qualica đang làm việc tại Nhật Bản.</p>
+						</div>
+					</li>
+					<li>
+						<img src="images/news/new3.jpg" alt="Ảnh DEMO">
+						<div>
+							<h4><a href="">ĐH Thủy lợi dự kiến mở một số ngành “hot” trong những năm tới đây</a></h4>
+							<p>Tới đây, Trường Đại học Thủy lợi dự kiến sẽ đào tạo một số ngành học đang có nhu cầu lớn trong xã hội nhằm cung cấp nguồn nhân lực chất lượng tốt dựa trên những điều kiện đảm bảo đáp ứng yêu cầu về...</p>
+						</div>
+					</li>
+					<li>
+						<img src="images/news/new4.jpg" alt="Ảnh DEMO">
+						<div>
+							<h4><a href="">Đại học Thủy lợi giành 8 giải thưởng Đồ án xuất sắc - Giải Loa thành năm 2019</a></h4>
+							<p>Trong tổng số 13 đồ án tham gia giải thưởng Loa Thành năm nay, Trường Đại học Thủy lợi có 8 đồ án đạt giải. Đồ án "Nghiên cứu mô hình dự báo lũ sông Kone" của Trần Thị Tuyết 56G do PGS.TS Ngô Lê An...</p>
+						</div>
+					</li>
+				</ul>
+			</div>
