5f879e142addab08d33aaf2296b61c5727d0c6b5
DangNguyen-maker <github.com/DangNguyen-maker/1551060773.git>
2019-12-18 10:49:59 +0700

    Them file footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+<div class="nav-footer">
+				<ul>
+					<li><a href="#">Giới thiệu</a></li>
+					<li><a href="#">Logo của Khoa CNTT</a></li>
+					<li><a href="#">Lời chào mừng</a></li>
+					<li><a href="#">Tổ chức</a></li>
+					<li><a href="#">Hợp tác liên kết</a></li>
+				</ul>
+				<ul>
+					<li><a href="#">Đào tạo</a></li>
+					<li><a href="#">Mô hình đào tạo</a></li>
+					<li><a href="#">Đào tạo đại học</a></li>
+					<li><a href="#">Định hướng ngành nghề</a></li>
+					<li><a href="#">Đào tạo sau đại học</a></li>
+				</ul>
+				<ul>
+					<li><a href="#">Nghiên cứu khoa học</a></li>
+					<li><a href="#">Thông tin seminar</a></li>
+					<li><a href="#">Các đề tài, dự án</a></li>
+					<li><a href="#">Công trình công bố</a></li>
+					<li><a href="#">Các phòng thí nghiệm</a></li>
+				</ul>
+				<ul>
+					<li><a href="#">Công nghệ phần mềm</a></li>
+					<li><a href="#">Hệ thống thông tin</a></li>
+					<li><a href="#">Khoa học máy tính</a></li>
+					<li><a href="#">Kỹ thuật máy tính và mạng</a></li>
+					<li><a href="#">Toán học</a></li>
+					<li><a href="#">Trung tâm tin học</a></li>
+				</ul>
+			</div>
