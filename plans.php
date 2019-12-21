6b8ea0b03f21e701b5c539ebbb92d827125e7533
DangNguyen-maker <github.com/DangNguyen-maker/1551060773.git>
2019-12-21 19:49:52 +0700

    Them file plans.php

---
+++ plans.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="css/style.css">
+	<title>News</title>
+</head>
+
+<body>
+	<div class="wrapper">
+		<header>
+			<?php
+			include 'header.php';
+			?>
+		</header>
+		<div class="main">
+			<div class="container news">
+				<h3>Kế hoạch giảng dạy CSE</h3>
+				<ul>
+					<li>
+						<img src="images/plans/plan1.jpg" alt="Ảnh Plan1">
+						<div>
+							<h4><a href="#">Thông báo về kế hoạch học kỳ 2 năm học 2019-2020 cho Chương trình tiên tiến </a></h4>
+							<p>Thông báo về kế hoạch học kỳ 2 năm học 2019-2020 cho Chương trình tiên tiến</p>
+						</div>
+					</li>
+					<li>
+						<img src="images/plans/plan2.jpg" alt="Ảnh Plan2">
+						<div>
+							<h4><a href="#">Thông báo thi chuẩn Tiếng Anh đâu ra cho trình độ đại học hệ chính quy đợt 4 năm 2019 </a></h4>
+							<p>Thông báo thi chuẩn Tiếng Anh đâu ra cho trình độ đại học hệ chính quy đợt 4 năm 2019</p>
+						</div>
+					</li>
+					<li>
+						<img src="images/plans/plan3.jpg" alt="Ảnh Plan3">
+						<div>
+							<h4><a href="#">Thông báo về kế hoạch học kỳ 2 năm học 2019-2020 </a></h4>
+							<p>Thông báo về kế hoạch học kỳ 2 năm học 2019-2020</p>
+						</div>
+					</li>
+					<li>
+						<img src="images/plans/plan1.jpg" alt="Ảnh Plan1">
+						<div>
+							<h4><a href="#">Quyết định Chuẩn đầu ra Ngoại ngữ cho K61 và học phần Tiếng Anh tăng cường </a></h4>
+							<p>Quyết định Chuẩn đầu ra Ngoại ngữ cho K61 và học phần Tiếng Anh tăng cường</p>
+						</div>
+					</li>
+					<li>
+						<img src="images/plans/plan4.jpg" alt="Ảnh Plan4">
+						<div>
+							<h4><a href="#">Thông báo triển khai kế hoạch học kỳ hè năm học 2018-2019 </a></h4>
+							<p>Thông báo số 444/TB-ĐHTL ngày 24/05/2019 về triển khai kế hoạch học kỳ hè năm học 2018-2019</p>
+						</div>
+					</li>
+					<li>
+						<img src="images/plans/plan3.jpg" alt="Ảnh Plan3">
+						<div>
+							<h4><a href="#">Thông báo triển khai kế hoạch tính toán khối lượng vượt giờ năm học 2018-2019 </a></h4>
+							<p>Thông báo triển khai kế hoạch tính toán khối lượng vượt giờ năm học 2018-2019</p>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<footer>
+			<?php
+			include 'footer.php';
+			?>
+		</footer>
+	</div>
+</body>
+
+</html>
